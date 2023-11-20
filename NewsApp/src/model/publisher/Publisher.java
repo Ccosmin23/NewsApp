599eb79e9cf727ab6ffe049b419b6c508b37ebde
Cosmin Cosan <cosan_cosmin@yahoo.com>
2023-11-20 20:48:57 +0200

    added some packages + publisher & subscriber interfaces

---
+++ NewsApp/src/model/publisher/Publisher.java
@@ -0,0 +1,11 @@
+package model.publisher;
+
+import model.User;
+
+public class Publisher extends User {
+    public Publisher(Integer id, String firstName, String lastName, Integer age, String country) {
+        super(id, firstName, lastName, age, country);
+    }
+
+
+}
---
+++ NewsApp/src/model/subscriber/Subscriber.java
@@ -0,0 +1,9 @@
+package model.subscriber;
+
+import model.User;
+
+public class Subscriber extends User {
+    public Subscriber(Integer id, String firstName, String lastName, Integer age, String country) {
+        super(id, firstName, lastName, age, country);
+    }
+}
