37f9595fe547766024938fa87fdacf192722010d
1-REDD-1 <100238439+1-REDD-1@users.noreply.github.com>
2023-11-20 11:28:16 +0200

    Adaugă interfața pentru abonat

---
+++ NewsApp/src/ui/InterfataAbonat.java
@@ -0,0 +1,51 @@
+package ui;
+
+import java.util.Scanner;
+
+public class InterfataAbonat {
+    public Scanner scannerTastatura;
+
+    public void afiseazaInterfata () {
+        String optiune;
+
+        scannerTastatura = new Scanner(System.in);
+
+        System.out.println("Optiunile disponibile sunt:\n");
+        System.out.println("[a] -> abonare la un topic");
+        System.out.println("[f] -> aflati ultimele stiri");
+        System.out.println("[x] -> iesire din program\n");
+
+        System.out.print("Optiunea dorita: ");
+        optiune = scannerTastatura.nextLine();
+
+        switch (optiune) {
+            case "a": {
+                afisareStiri();
+                break;
+            }
+
+            case "f": {
+                abonareLaTopic();
+                break;
+            }
+
+            case "x": {
+                // Închidere program
+            }
+        }
+    }
+
+    public void afisareStiri () {
+
+    }
+
+    public void abonareLaTopic () {
+
+    }
+
+    public void inchideInterfata () {
+        scannerTastatura.close();
+    }
+
+    public InterfataPublisher () {}
+}
