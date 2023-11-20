a1ea0a1ed38a1e969976fd94f33b0a497dd79b2d
Cosmin Cosan <cosan_cosmin@yahoo.com>
2023-11-20 22:57:27 +0200

    added subscriber interface + rename ring architecture

--- NewsApp/src/model/subscriber/Subscriber.java
+++ NewsApp/src/model/subscriber/Subscriber.java
@@ -2,8 +2,13 @@ package model.subscriber;
 
 import model.User;
 
-public class Subscriber extends User {
+public class Subscriber extends User implements SubscriberInterface {
     public Subscriber(Integer id, String firstName, String lastName, Integer age, String country) {
         super(id, firstName, lastName, age, country);
+    }
+
+    @Override
+    public void receiveNews() {
+        
     }
 }
