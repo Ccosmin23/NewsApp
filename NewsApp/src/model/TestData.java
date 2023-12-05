6b24d690e0fc8736559b104b2eda5b6479bd1125
1-REDD-1 <100238439+1-REDD-1@users.noreply.github.com>
2023-12-05 09:03:28 +0200

    Adaugă proprietatea de comandă la TestData (test)
    Este folositoare pentru comunicare atât între abonați
    și brokeri, cât și între publicatori și brokeri.

--- NewsApp/src/model/TestData.java
+++ NewsApp/src/model/TestData.java
@@ -7,6 +7,7 @@ public class TestData implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private String mesaj;
+    private String comanda;
     private InetAddress destinatie;
 
     public String primesteMesaj () {
@@ -17,8 +18,17 @@ public class TestData implements Serializable {
         return this.destinatie;
     }
 
+    public void seteazaComanda (String comandaData) {
+        this.comanda = comandaData;
+    }
+
+    public String primesteComanda () {
+        return this.comanda;
+    }
+
     public TestData (String msg, InetAddress dest) {
         this.destinatie = dest;
         this.mesaj = msg;
+        this.comanda = "";
     }
 }
