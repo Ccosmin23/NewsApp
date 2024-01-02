612e9e1e160909a2a50bcbd78d0f83cfb30ca9e3
Cosmin Cosan <cosan_cosmin@yahoo.com>
2024-01-02 15:49:45 +0200

    refactoring Main

--- NewsApp/src/Main.java
+++ NewsApp/src/Main.java
@@ -10,65 +10,72 @@ import architecture.Subscriber;
 public class Main {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Trebuie specificat unul dintre argumentele date:");
-            System.out.println("    [publisher] -> lanseaza procesul pentru abonat");
-            System.out.println("    [subscriber] -> lanseaza procesul pentru publisher");
-            System.out.println("    [broker] -> lanseaza procesul pentru broker, care se ocupa cu stocarea si dispecerizarea mesajelor");
+            showMenuMessages();
             return;
         }
 
+        startAppWith(args);
+    }
+
+    public static void startAppWith(String[] args) {
         switch (args[0]) {
-            case "publisher": {
-                Publisher masina = new Publisher();
-
-                try {
-                    masina.start();
-                } catch (UnknownHostException | ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                break;
-            }
-
-            case "subscriber": {
-                Subscriber masina = new Subscriber();
-                
-                try {
-                    masina.start();
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                }
-                
-                break;
-            }
-
-            case "broker": {
-                ArrayList<InetAddress> adreseRetea = new ArrayList<>();
-
-                try {
-                    adreseRetea.add(InetAddress.getByName("192.168.30.4"));
-                    adreseRetea.add(InetAddress.getByName("192.168.30.7"));
-                    adreseRetea.add(InetAddress.getByName("192.168.30.9"));
-                    adreseRetea.add(InetAddress.getByName("192.168.30.10"));
-                    adreseRetea.add(InetAddress.getByName("192.168.30.12"));
-
-                    Broker masina = new Broker(adreseRetea);
-                    masina.start();
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                } catch (SocketException e) {
-                    e.printStackTrace();
-                }
-                break;
-            }
-
+            case "publisher":
+                startAsAPublisher();
+            case "subscriber":
+                startAsASubscriber();
+            case "broker":
+                startAsABroker();
             default: {
-                System.out.println("Trebuie specificat unul dintre argumentele date:");
-                System.out.println("    [publisher] -> lanseaza procesul pentru abonat");
-                System.out.println("    [subscriber] -> lanseaza procesul pentru publisher");
-                System.out.println("    [broker] -> lanseaza procesul pentru broker, care se ocupa cu stocarea si dispecerizarea mesajelor");
+                showMenuMessages();
                 break;
             }
+        }
+    }
+
+    public static void showMenuMessages() {
+        System.out.println("We cannot start the app for you, because you should enter an argument as:" +
+                "\t'publisher', 'subscribe' or 'broker'\n\n" +
+                "\tpublisher will start the execution as a publisher" +
+                "\tsubscriber will start the execution as a subscriber" +
+                "\tbroker -> will start the execution as a broker");
+    }
+
+    public static void startAsAPublisher() {
+        Publisher masina = new Publisher();
+
+        try {
+            masina.start();
+        } catch (UnknownHostException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void startAsASubscriber() {
+        Subscriber masina = new Subscriber();
+
+        try {
+            masina.start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void startAsABroker() {
+        ArrayList<InetAddress> adreseRetea = new ArrayList<>();
+
+        try {
+            adreseRetea.add(InetAddress.getByName("192.168.30.4"));
+            adreseRetea.add(InetAddress.getByName("192.168.30.7"));
+            adreseRetea.add(InetAddress.getByName("192.168.30.9"));
+            adreseRetea.add(InetAddress.getByName("192.168.30.10"));
+            adreseRetea.add(InetAddress.getByName("192.168.30.12"));
+
+            Broker masina = new Broker(adreseRetea);
+            masina.start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketException e) {
+            e.printStackTrace();
         }
     }
 }
