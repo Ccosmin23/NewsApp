48d4f63aa2555c41930beb0689ec8cd86449c281
Cosmin Cosan <cosan_cosmin@yahoo.com>
2024-01-04 19:52:13 +0200

    split into multiple methods

--- NewsApp/src/service/BrokerService.java
+++ NewsApp/src/service/BrokerService.java
@@ -22,10 +22,10 @@ import model.news.NewsField;
 public final class BrokerService {
     InetAddress adresaPersonala;
     InetAddress nodUrmator;
-    
+
     ServerSocket receiverSocket;
     ArrayList<InetAddress> adreseNoduri;
-    AtomicBoolean ruleaza;
+    AtomicBoolean programIsRunning;
     NewsField listaStiri;
 
     public static BrokerService shared = new BrokerService();
@@ -35,9 +35,49 @@ public final class BrokerService {
         this.listaStiri = new NewsField(1, "Stiri");
     }
 
-    public void start () throws SocketException, UnknownHostException {
-        Enumeration<NetworkInterface> interfeteRetea = NetworkInterface.getNetworkInterfaces();
+    // ========================================== start() ==========================================
+    public void start() throws UnknownHostException, SocketException {
+        findSuitableHostAddress();
+        receive().start();
+        userInputHandler();
+        System.out.println("\nS-a incheiat executia pentru acest broker!");
+    }
+
+    private void userInputHandler() throws UnknownHostException {
+        while (this.programIsRunning.get() == true) {
+            Console consola = System.console();
+            switch (consola.readLine("-> ")) {
+                case "s": {
+                    try {
+                        send(InetAddress.getByName("192.168.30.10"));
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+
+                case "x": {
+                    this.programIsRunning.set(false);
+
+                    try {
+                        receiverSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+                }
+
+                default: {
+                    break;
+                }
+            }
+        }
+    }
+
+    private InetAddress findSuitableHostAddress() throws SocketException, UnknownHostException {
         InetAddress adresaGazda = null;
+        Enumeration<NetworkInterface> interfeteRetea = NetworkInterface.getNetworkInterfaces();
 
         while (interfeteRetea.hasMoreElements()) {
             NetworkInterface networkInterface = interfeteRetea.nextElement();
@@ -66,40 +106,139 @@ public final class BrokerService {
             throw new IllegalStateException("nu se poate gasi o adresa gazda");
         }
 
-        this.ruleaza = new AtomicBoolean(true);
-        receive().start();
-        while (this.ruleaza.get() == true) {
-            Console consola = System.console();
-            switch (consola.readLine("-> ")) {
-                case "s": {
-                    try {
-                        send(InetAddress.getByName("192.168.30.10"));
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                }
-
-                case "x": {
-                    this.ruleaza.set(false);
-                    try {
-                        receiverSocket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    break;
-                }
-
-                default: {
-                    break;
-                }
-            }
-        }
-
-        System.out.println("AM TERMINAT!");
+        return adresaGazda;
     }
 
+    // ========================================== receive() ==========================================
+    public Thread receive () {
+        this.programIsRunning = new AtomicBoolean(true);
+
+        return new Thread(new Runnable() {
+            @Override
+            public void run () {
+                Socket clientSocket;
+                DataInputStream clientIStream;
+                DataOutputStream clientOStream;
+                ObjectOutputStream oos;
+                ObjectInputStream ois;
+                BrokerMessage mesajReceptionat;
+
+                try {
+                    receiverSocket = new ServerSocket(9700);
+                    while (programIsRunning.get() == true) {
+                        try {
+                            clientSocket = receiverSocket.accept();
+                            System.out.println("Mesajul de la " + clientSocket.getInetAddress().toString() + " este receptionat!");
+
+                            clientIStream = new DataInputStream(clientSocket.getInputStream());
+                            clientOStream = new DataOutputStream(clientSocket.getOutputStream());
+
+                            ois = new ObjectInputStream(clientIStream);
+                            oos = new ObjectOutputStream(clientOStream);
+
+                            mesajReceptionat = (BrokerMessage) ois.readObject();
+
+                            System.out.println(mesajReceptionat.primesteComanda());
+
+                            try {
+                                process(mesajReceptionat, oos, ois, clientSocket);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                            ois.close();
+                            oos.close();
+                            clientSocket.close();
+
+                        } catch (SocketException e) {
+                            System.out.println(e.getMessage());
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    System.out.println("AM ÎNCHEIAT ASCULTAREA!");
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void process(BrokerMessage mesajReceptionat, ObjectOutputStream oos, ObjectInputStream ois, Socket clientSocket) throws IOException, ClassNotFoundException, InterruptedException {
+        switch (mesajReceptionat.primesteComanda()) {
+            case "publica":
+                publish(mesajReceptionat, oos);
+                break;
+            case "articole":
+                provideNews(oos);
+                break;
+            case "replicare":
+                replicate(mesajReceptionat, oos, clientSocket);
+                break;
+            default:
+                handleDefaultCase(mesajReceptionat, oos);
+        }
+    }
+
+    private void publish(BrokerMessage mesajReceptionat, ObjectOutputStream oos) throws ClassNotFoundException, IOException {
+        BrokerMessage raspuns = new BrokerMessage("Ti-am receptionat publicarea!", adresaPersonala);
+        BrokerMessage replica = new BrokerMessage("Replica mesaj, replicare articol!", adresaPersonala);
+
+        System.out.println("Am primit mesaj de la PUBLISHER (publicator)!");
+        listaStiri.adaugaStire(mesajReceptionat.primesteStirea());
+        oos.writeObject(raspuns);
+
+        System.out.println("Aduc si la restul sistemului articolul introdus.");
+        replica.seteazaComanda("replicare");
+        replica.seteazaStirea(mesajReceptionat.primesteStirea());
+
+        replicaArticolLaVecin(replica);
+    }
+
+    private void provideNews(ObjectOutputStream oos) throws IOException {
+        BrokerMessage raspuns = new BrokerMessage("Ti-am receptionat nevoia de date (articolele in acest caz)!", adresaPersonala);
+
+        raspuns.seteazaListaStiri(listaStiri);
+
+        System.out.println("Am primit mesaj de la SUBSCRIBER (abonat)!");
+        oos.writeObject(raspuns);
+    }
+
+    private void replicate(BrokerMessage mesajReceptionat, ObjectOutputStream oos, Socket clientSocket) throws IOException, ClassNotFoundException {
+        BrokerMessage raspuns = new BrokerMessage("Am replicat stirea \"" + mesajReceptionat.primesteStirea().getTitlu() + "\"", adresaPersonala);
+
+        System.out.println("Am primit cerere de replicare de la broker-ul vecin " + clientSocket.getInetAddress().toString());
+
+        listaStiri.adaugaStire(mesajReceptionat.primesteStirea());
+        oos.writeObject(raspuns);
+
+        if (mesajReceptionat.primesteAdresa().equals(nodUrmator) != true) {
+            System.out.println("Îl replic articolul la următorul vecin.");
+            replicaArticolLaVecin(mesajReceptionat);
+        } else {
+            System.out.println("Nu mai replic și la nodul originar.");
+        }
+    }
+
+    private void handleDefaultCase(BrokerMessage mesajReceptionat, ObjectOutputStream oos) throws ClassNotFoundException, IOException, InterruptedException {
+        if (mesajReceptionat.primesteAdresa().equals(adresaPersonala)) {
+            BrokerMessage raspuns = new BrokerMessage("Sunt eu. Mulțumesc pentru mesaj!", adresaPersonala);
+
+            System.out.println("Sunt eu!");
+            oos.writeObject(raspuns);
+        } else {
+            BrokerMessage raspuns = new BrokerMessage("Nu sunt eu. Însă am trimis mai departe", adresaPersonala);
+
+            System.out.println("Nu sunt eu!");
+            oos.writeObject(raspuns);
+            Thread.sleep(500);
+            send(mesajReceptionat.primesteAdresa());
+        }
+    }
+
+    // ========================================== send() ==========================================
     public void send (InetAddress destinatie) throws ClassNotFoundException {
         try {
             ObjectOutputStream oos;
@@ -151,116 +290,6 @@ public final class BrokerService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public Thread receive () {
-        return new Thread(new Runnable() {
-            @Override
-            public void run () {
-                Socket clientSocket;
-                DataInputStream clientIStream;
-                DataOutputStream clientOStream;
-                ObjectOutputStream oos;
-                ObjectInputStream ois;
-                BrokerMessage mesajReceptionat;
-
-                try {
-                    receiverSocket = new ServerSocket(9700);
-                    while (ruleaza.get() == true) {
-                        try {
-                            clientSocket = receiverSocket.accept();
-                            System.out.println("Mesajul de la " + clientSocket.getInetAddress().toString() + " este receptionat!");
-
-                            clientIStream = new DataInputStream(clientSocket.getInputStream());
-                            clientOStream = new DataOutputStream(clientSocket.getOutputStream());
-
-                            ois = new ObjectInputStream(clientIStream);
-                            oos = new ObjectOutputStream(clientOStream);
-
-                            mesajReceptionat = (BrokerMessage) ois.readObject();
-
-                            System.out.println(mesajReceptionat.primesteComanda());
-                            switch (mesajReceptionat.primesteComanda()) {
-                                case "publica": {
-                                    BrokerMessage raspuns = new BrokerMessage("Ti-am receptionat publicarea!", adresaPersonala);
-                                    BrokerMessage replica = new BrokerMessage("Replica mesaj, replicare articol!", adresaPersonala);
-
-                                    System.out.println("Am primit mesaj de la PUBLISHER (publicator)!");
-                                    listaStiri.adaugaStire(mesajReceptionat.primesteStirea());
-                                    oos.writeObject(raspuns);
-
-                                    System.out.println("Aduc si la restul sistemului articolul introdus.");
-                                    replica.seteazaComanda("replicare");
-                                    replica.seteazaStirea(mesajReceptionat.primesteStirea());
-
-                                    replicaArticolLaVecin(replica);
-
-                                    break;
-                                }
-
-                                case "articole": {
-                                    BrokerMessage raspuns = new BrokerMessage("Ti-am receptionat nevoia de date (articolele in acest caz)!", adresaPersonala);
-
-                                    raspuns.seteazaListaStiri(listaStiri);
-
-                                    System.out.println("Am primit mesaj de la SUBSCRIBER (abonat)!");
-                                    oos.writeObject(raspuns);
-                                    break;
-                                }
-
-                                case "replicare": {
-                                    BrokerMessage raspuns = new BrokerMessage("Am replicat stirea \"" + mesajReceptionat.primesteStirea().getTitlu() + "\"", adresaPersonala);
-
-                                    System.out.println("Am primit cerere de replicare de la broker-ul vecin " + clientSocket.getInetAddress().toString());
-
-                                    listaStiri.adaugaStire(mesajReceptionat.primesteStirea());
-                                    oos.writeObject(raspuns);
-
-                                    if (mesajReceptionat.primesteAdresa().equals(nodUrmator) != true) {
-                                        System.out.println("Îl replic articolul la următorul vecin.");
-                                        replicaArticolLaVecin(mesajReceptionat);
-                                    } else {
-                                        System.out.println("Nu mai replic și la nodul originar.");
-                                    }
-
-                                    break;
-                                }
-
-                                default: {
-                                    if (mesajReceptionat.primesteAdresa().equals(adresaPersonala)) {
-                                        BrokerMessage raspuns = new BrokerMessage("Sunt eu. Mulțumesc pentru mesaj!", adresaPersonala);
-
-                                        System.out.println("Sunt eu!");
-                                        oos.writeObject(raspuns);
-                                    } else {
-                                        BrokerMessage raspuns = new BrokerMessage("Nu sunt eu. Însă am trimis mai departe", adresaPersonala);
-
-                                        System.out.println("Nu sunt eu!");
-                                        oos.writeObject(raspuns);
-                                        Thread.sleep(500);
-                                        send(mesajReceptionat.primesteAdresa());
-                                    }
-                                }
-                            }
-
-                            ois.close();
-                            oos.close();
-                            clientSocket.close();
-                        } catch (SocketException e) {
-                            System.out.println(e.getMessage());
-                        } catch (ClassNotFoundException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    System.out.println("AM ÎNCHEIAT ASCULTAREA!");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 
     private ArrayList<InetAddress> getInetAddresses() {
