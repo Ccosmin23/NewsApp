296c359d65d6913d5315b35b23f98d202dba8921
1-REDD-1 <100238439+1-REDD-1@users.noreply.github.com>
2024-01-08 21:32:17 +0200

    Add random string generator
    The added string generator method is useful for generating
    a string of a random length. It is very useful for creating
    random news articles.

--- NewsApp/src/utils/StringUtils.java
+++ NewsApp/src/utils/StringUtils.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.Random;
+
 public class StringUtils {
     public static String applyBoldTo(String text, Boolean withSingleQuotationMarks) {
         String textToReturn;
@@ -11,5 +13,23 @@ public class StringUtils {
         }
 
         return textToReturn;
+    }
+
+    //   Generează un șir de caractere cu lungime cuprins în intervalul ["minSize", "maxSize"]
+    // și conține caractere de la 'a' la 'z'
+    public static String randomString (int minSize, int maxSize) {
+        StringBuilder textToReturn = new StringBuilder("");
+        Random rng = new Random();
+        
+        int i = 0;
+        int length = rng.nextInt(minSize, maxSize);
+
+        while (i < length) {
+            textToReturn.append((char) rng.nextInt(0x61, 0x7A));
+            
+            i += 1;
+        }
+
+        return textToReturn.toString();
     }
 }
